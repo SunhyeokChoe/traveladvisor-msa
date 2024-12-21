76106adab3535a48245cb8c002d466c0ed0c8c27
sunhyeokchoe <sunhyeokchoe@gmail.com>
2024-12-21 20:24:05 +0900

    feat(hotel-domain-core): 호텔 예약 처리 결과 이벤트 구현

---
+++ hotelserver/hotel-domain/hotel-domain-core/src/main/java/com/traveladvisor/hotelserver/service/domain/event/BookingApprovalEvent.java
@@ -0,0 +1,35 @@
+package com.traveladvisor.hotelserver.service.domain.event;
+
+import com.traveladvisor.common.domain.event.DomainEvent;
+import com.traveladvisor.hotelserver.service.domain.entity.BookingApproval;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public abstract class BookingApprovalEvent implements DomainEvent<BookingApproval> {
+
+    private final BookingApproval bookingApproval;
+    private final List<String> failureMessages;
+    private final ZonedDateTime createdAt;
+
+    protected BookingApprovalEvent(BookingApproval bookingApproval,
+                                   List<String> failureMessages,
+                                   ZonedDateTime createdAt) {
+        this.bookingApproval = bookingApproval;
+        this.failureMessages = failureMessages;
+        this.createdAt = createdAt;
+    }
+
+    public BookingApproval getBookingApproval() {
+        return bookingApproval;
+    }
+
+    public List<String> getFailureMessages() {
+        return failureMessages;
+    }
+
+    public ZonedDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+}
---
+++ hotelserver/hotel-domain/hotel-domain-core/src/main/java/com/traveladvisor/hotelserver/service/domain/event/HotelBookingApprovedEvent.java
@@ -0,0 +1,17 @@
+package com.traveladvisor.hotelserver.service.domain.event;
+
+import com.traveladvisor.hotelserver.service.domain.entity.BookingApproval;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+/**
+ * 호텔 예약 완료 이벤트 입니다.
+ */
+public class HotelBookingApprovedEvent extends BookingApprovalEvent {
+
+    public HotelBookingApprovedEvent(BookingApproval bookingApproval, List<String> failureMessages, ZonedDateTime createdAt) {
+        super(bookingApproval, failureMessages, createdAt);
+    }
+
+}
