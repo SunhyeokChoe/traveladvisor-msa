3ad0da76e095da0325981eeefea899f7e2f930c0
sunhyeokchoe <sunhyeokchoe@gmail.com>
2024-12-21 19:49:22 +0900

    feat(flight-container): 스키마 및 시딩 데이터 추가

---
+++ flightserver/flight-container/src/main/resources/init-data.sql
@@ -0,0 +1,45 @@
+INSERT INTO flight.flight_offers (carrier_code, operating_carrier_code, flight_number, aircraft_code, departure_iata, departure_at, arrival_iata, arrival_at, duration, blacklisted_in_eu, number_of_stops, price)
+VALUES
+    ('6X', '6X', '3618', '733', 'CDG', '2024-12-30T15:25:00+00', 'HEL', '2024-12-30T19:20:00+00', INTERVAL '2 hours 55 minutes', false, 0, 399.44);
+INSERT INTO flight.flight_offers (carrier_code, operating_carrier_code, flight_number, aircraft_code, departure_iata, departure_at, arrival_iata, arrival_at, duration, blacklisted_in_eu, number_of_stops, price)
+VALUES
+    ('6X', '6X', '3605', '733', 'HEL', '2024-12-31T17:30:00+00', 'ICN', '2025-01-01T08:20:00+00', INTERVAL '7 hours 50 minutes', false, 0, 505.29);
+INSERT INTO flight.flight_offers (carrier_code, operating_carrier_code, flight_number, aircraft_code, departure_iata, departure_at, arrival_iata, arrival_at, duration, blacklisted_in_eu, number_of_stops, price)
+VALUES
+    ('6X', '6X', '957', '332', 'ICN', '2025-01-13T09:53:00+00', 'CDG', '2025-01-13T14:10:00+00', INTERVAL '11 hours 17 minutes', false, 0, 363.14);
+INSERT INTO flight.flight_offers (carrier_code, operating_carrier_code, flight_number, aircraft_code, departure_iata, departure_at, arrival_iata, arrival_at, duration, blacklisted_in_eu, number_of_stops, price)
+VALUES
+    ('6X', '6X', '9978', '319', 'CDG', '2024-12-30T15:25:00+00', 'HEL', '2024-12-30T19:20:00+00', INTERVAL '2 hours 55 minutes', false, 0, 606.39);
+INSERT INTO flight.flight_offers (carrier_code, operating_carrier_code, flight_number, aircraft_code, departure_iata, departure_at, arrival_iata, arrival_at, duration, blacklisted_in_eu, number_of_stops, price)
+VALUES
+    ('6X', '6X', '3605', '733', 'HEL', '2024-12-31T17:30:00+00', 'ICN', '2025-01-01T08:20:00+00', INTERVAL '7 hours 50 minutes', false, 0, 391.33);
+INSERT INTO flight.flight_offers (carrier_code, operating_carrier_code, flight_number, aircraft_code, departure_iata, departure_at, arrival_iata, arrival_at, duration, blacklisted_in_eu, number_of_stops, price)
+VALUES
+    ('6X', '6X', '957', '332', 'ICN', '2025-01-13T09:53:00+00', 'CDG', '2025-01-13T14:10:00+00', INTERVAL '11 hours 17 minutes', false, 0, 782.11);
+INSERT INTO flight.flight_offers (carrier_code, operating_carrier_code, flight_number, aircraft_code, departure_iata, departure_at, arrival_iata, arrival_at, duration, blacklisted_in_eu, number_of_stops, price)
+VALUES
+    ('6X', '6X', '3618', '733', 'CDG', '2024-12-30T15:25:00+00', 'HEL', '2024-12-30T19:20:00+00', INTERVAL '2 hours 55 minutes', false, 0, 500.00);
+INSERT INTO flight.flight_offers (carrier_code, operating_carrier_code, flight_number, aircraft_code, departure_iata, departure_at, arrival_iata, arrival_at, duration, blacklisted_in_eu, number_of_stops, price)
+VALUES
+    ('6X', '6X', '3605', '733', 'HEL', '2024-12-31T17:30:00+00', 'ICN', '2025-01-01T08:20:00+00', INTERVAL '7 hours 50 minutes', false, 0, 650.92);
+INSERT INTO flight.flight_offers (carrier_code, operating_carrier_code, flight_number, aircraft_code, departure_iata, departure_at, arrival_iata, arrival_at, duration, blacklisted_in_eu, number_of_stops, price)
+VALUES
+    ('6X', '6X', '956', '332', 'ICN', '2025-01-13T14:05:00+00', 'CDG', '2025-01-13T18:35:00+00', INTERVAL '11 hours 30 minutes', false, 0, 369.99);
+INSERT INTO flight.flight_offers (carrier_code, operating_carrier_code, flight_number, aircraft_code, departure_iata, departure_at, arrival_iata, arrival_at, duration, blacklisted_in_eu, number_of_stops, price)
+VALUES
+    ('6X', '6X', '9978', '319', 'CDG', '2024-12-30T15:25:00+00', 'HEL', '2024-12-30T19:20:00+00', INTERVAL '2 hours 55 minutes', false, 0, 490.99);
+INSERT INTO flight.flight_offers (carrier_code, operating_carrier_code, flight_number, aircraft_code, departure_iata, departure_at, arrival_iata, arrival_at, duration, blacklisted_in_eu, number_of_stops, price)
+VALUES
+    ('6X', '6X', '3605', '733', 'HEL', '2024-12-31T17:30:00+00', 'ICN', '2025-01-01T08:20:00+00', INTERVAL '7 hours 50 minutes', false, 0, 580.19);
+INSERT INTO flight.flight_offers (carrier_code, operating_carrier_code, flight_number, aircraft_code, departure_iata, departure_at, arrival_iata, arrival_at, duration, blacklisted_in_eu, number_of_stops, price)
+VALUES
+    ('6X', '6X', '956', '332', 'ICN', '2025-01-13T14:05:00+00', 'CDG', '2025-01-13T18:35:00+00', INTERVAL '11 hours 30 minutes', false, 0, 649.88);
+INSERT INTO flight.flight_offers (carrier_code, operating_carrier_code, flight_number, aircraft_code, departure_iata, departure_at, arrival_iata, arrival_at, duration, blacklisted_in_eu, number_of_stops, price)
+VALUES
+    ('6X', '6X', '501', '744', 'CDG', '2024-12-30T10:00:00+00', 'FRA', '2024-12-30T14:30:00+00', INTERVAL '4 hours 30 minutes', false, 0, 530.13);
+INSERT INTO flight.flight_offers (carrier_code, operating_carrier_code, flight_number, aircraft_code, departure_iata, departure_at, arrival_iata, arrival_at, duration, blacklisted_in_eu, number_of_stops, price)
+VALUES
+    ('6X', '6X', '9744', '744', 'FRA', '2024-12-30T18:10:00+00', 'ICN', '2024-12-31T11:25:00+00', INTERVAL '10 hours 15 minutes', false, 0, 580.76);
+INSERT INTO flight.flight_offers (carrier_code, operating_carrier_code, flight_number, aircraft_code, departure_iata, departure_at, arrival_iata, arrival_at, duration, blacklisted_in_eu, number_of_stops, price)
+VALUES
+    ('6X', '6X', '957', '332', 'ICN', '2025-01-13T09:53:00+00', 'CDG', '2025-01-13T14:10:00+00', INTERVAL '11 hours 17 minutes', false, 0, 793.99);
