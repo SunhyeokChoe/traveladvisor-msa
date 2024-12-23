1f9c3631d77ec29004c68a4257522cb3340c7098
sunhyeokchoe <sunhyeokchoe@gmail.com>
2024-12-23 11:42:36 +0900

    feat(common-domain): HotelBookingApprovalStatus 추가, HotelBookingStatus 상태 변경

---
+++ common/common-domain/src/main/java/com/traveladvisor/common/domain/vo/HotelBookingApprovalStatus.java
@@ -0,0 +1,20 @@
+package com.traveladvisor.common.domain.vo;
+
+public enum HotelBookingApprovalStatus {
+
+    /**
+     * 호텔 예약 완료
+     */
+    COMPLETED,
+
+    /**
+     * 호텔 예약 취소 완료
+     */
+    CANCELLED,
+
+    /**
+     * 호텔 예약 실패
+     */
+    FAILED,
+
+}
--- common/common-domain/src/main/java/com/traveladvisor/common/domain/vo/HotelBookingStatus.java
+++ common/common-domain/src/main/java/com/traveladvisor/common/domain/vo/HotelBookingStatus.java
@@ -3,18 +3,25 @@ package com.traveladvisor.common.domain.vo;
 public enum HotelBookingStatus {
 
     /**
+     * 호텔 예약 진행 중
+     */
+    PENDING,
+
+    /**
+     * 호텔 예약 취소 완료
+     */
+    CANCELLED,
+
+    // TODO: 삭제
+    /**
      * 호텔 예약 완료
      */
     HOTEL_BOOKED,
 
+    // TODO: 삭제
     /**
-     * 호텔 예약 취소
+     * 호텔 예약 실패
      */
-    HOTEL_CANCELLED,
-
-    /**
-     * 예약 실패
-     */
-    HOTEL_FAILED
+    HOTEL_REJECTED
 
 }
