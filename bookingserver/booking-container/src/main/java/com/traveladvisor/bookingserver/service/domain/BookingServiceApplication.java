f0609d0d0bd2d764309dfe037c8461277ba30131
sunhyeokchoe <sunhyeokchoe@gmail.com>
2024-12-28 11:07:16 +0900

    feat(booking-container): 패키지 경로 수정에 따른 코드 수정

--- bookingserver/booking-container/src/main/java/com/traveladvisor/bookingserver/service/domain/BookingServiceApplication.java
+++ bookingserver/booking-container/src/main/java/com/traveladvisor/bookingserver/service/domain/BookingServiceApplication.java
@@ -11,11 +11,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableDiscoveryClient
 // JPA 레포는 데이터 소스 모듈에 있습니다.
-@EnableJpaRepositories(basePackages = { "com.traveladvisor.booking.service.datasource", "com.traveladvisor.common.datasource" })
+@EnableJpaRepositories(basePackages = { "com.traveladvisor.bookingserver.service.datasource", "com.traveladvisor.common.datasource" })
 // Entity 또한 데이터 소스 모듈에 있습니다.
-@EntityScan(basePackages = { "com.traveladvisor.booking.service.datasource", "com.traveladvisor.common.datasource" })
-@EnableFeignClients(basePackages = "com.traveladvisor.booking.service.message.outbound.client")
-@SpringBootApplication(scanBasePackages = "com.traveladvisor.booking")
+@EntityScan(basePackages = { "com.traveladvisor.bookingserver.service.datasource", "com.traveladvisor.common.datasource" })
+@EnableFeignClients(basePackages = "com.traveladvisor.bookingserver.service.message.outbound.client")
+@SpringBootApplication(scanBasePackages = "com.traveladvisor.bookingserver")
 public class BookingServiceApplication {
 
     public static void main(String[] args) {
