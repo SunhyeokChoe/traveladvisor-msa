3951e3f4d551ced195d46224c36267d71de7d64b
sunhyeokchoe <sunhyeokchoe@gmail.com>
2025-01-07 18:06:44 +0900

    refactor(batch-application-service): 패키지 이름 수정
    query -> command

--- batchserver/batch-domain/batch-application-service/src/main/java/com/traveladvisor/batchserver/service/domain/usecase/command/BatchCreateHotelsJobHandler.java
+++ batchserver/batch-domain/batch-application-service/src/main/java/com/traveladvisor/batchserver/service/domain/usecase/command/BatchCreateHotelsJobHandler.java
@@ -1,4 +1,4 @@
-package com.traveladvisor.batchserver.service.domain.usecase.query;
+package com.traveladvisor.batchserver.service.domain.usecase.command;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
